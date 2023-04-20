d73d1054980e6ba8660b2c9bbe761bc3e4692b8b
Maggie Recio <m.reciogrom@gmail.com>
2023-04-20 09:19:30 +0100

    delete subrepo

--- client/src/components/MainPage.tsx
+++ client/src/components/MainPage.tsx
@@ -6,10 +6,23 @@ const MainPage: React.FC = () => {
     <>
       <NavBar />
       <FilterBar />
-      <div className='Hero'>
-        <div className='InnerHeroDiv'></div>
+      <div className="custom-shape-divider-bottom-1681978299">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+            className="shape-fill"
+          ></path>
+        </svg>
       </div>
-      <h2 className='text-3xl '>Main page content</h2>
+      <div className="Hero">
+        <div className="InnerHeroDiv"></div>
+      </div>
+      <h2 className="text-3xl ">Main page content</h2>
     </>
   );
 };
