908067906ee937b9f3d0c5c30fa8bfb5788a4c9c
Ahmad Ali <ahmad12893@hotmail.com>
2023-04-29 12:33:08 +0100

    end-to-end tests in one file

--- server/__tests__/tests.spec.ts
+++ server/__tests__/tests.spec.ts
@@ -2,10 +2,12 @@ import app from '../index';
 import supertest from 'supertest';
 import { deleteOne } from '../models/userModel';
 import { deleteAddress } from '../models/addressModel';
+import { deleteItem } from '../models/itemsModel';
 
 let userId: string;
 let addressId: string;
 let token: string;
+let itemId: string;
 
 describe('registration', () => {
   it('should register a user', async () => {
@@ -104,9 +106,96 @@ describe('address', () => {
   });
 });
 
-afterAll(async () => {
-  // console.log('Deleting user data for user ID:', userId);
-  await deleteOne(userId);
-  await deleteAddress(addressId);
-  // console.log('Result of deleteOne:', result);
+describe('Item Testing', () => {
+  it('should add an item', async () => {
+    const res = await supertest
+      .agent(app)
+      .post('/add-Item')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'test1',
+        desc: 'test1',
+        category: 'test1',
+        condition: 'test1',
+        price: 100,
+        size: 'test1',
+        img: 'test1',
+      });
+    expect(res.status).toBe(201);
+    itemId = res.body._id;
+  });
+
+  it('should edit an item ', async () => {
+    const res = await supertest
+      .agent(app)
+      .put(`/update-item/${itemId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        price: 9999,
+      });
+    expect(res.status).toBe(200);
+  });
+
+  it('should get all the items specific to a user', async () => {
+    const res = await supertest
+      .agent(app)
+      .get(`/user-items/${userId}`)
+      .set('Authorization', `Bearer ${token}`);
+    expect(res.status).toBe(200);
+  });
+
+  it('should get all the items', async () => {
+    const res = await supertest.agent(app).get('/get-Items');
+    expect(res.status).toBe(200);
+  });
+
+  it('should delete an item', async () => {
+    const res = await supertest.agent(app).delete(`/delete-item/${itemId}`);
+    expect(res.status).toBe(201);
+  });
+
+  it('should not add an item without authentication', async () => {
+    const res = await supertest.agent(app).post('/add-Item').send({
+      title: 'test1',
+      desc: 'test1',
+      category: 'test1',
+      condition: 'test1',
+      price: 100,
+      size: 'test1',
+      img: 'test1',
+    });
+    expect(res.status).toBe(403);
+  });
+
+  it('should not edit an item with invalid ID', async () => {
+    const res = await supertest
+      .agent(app)
+      .put('/update-item/invalidID')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        price: 9999,
+      });
+    expect(res.status).toBe(404);
+  });
+
+  it('should not get items specific to an invalid user', async () => {
+    const res = await supertest
+      .agent(app)
+      .get('/user-items/invalidID')
+      .set('Authorization', `Bearer ${token}`);
+    expect(res.status).toBe(404);
+  });
+
+  it('should not delete an invalid item', async () => {
+    const res = await supertest.agent(app).delete('/delete-item/invalidID');
+    expect(res.status).toBe(403);
+  });
+
+  afterAll(async () => {
+    // console.log('Deleting user data for user ID:', userId);
+    await deleteOne(userId);
+    await deleteAddress(addressId);
+    await deleteItem(itemId);
+    // console.log('Result of deleteOne:', result);
+  });
 });
