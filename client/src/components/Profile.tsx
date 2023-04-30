fbf174c6d78d7c4c6dbbf4dbb9ecb8058d4ea2ae
Ahmad Ali <ahmad12893@hotmail.com>
2023-04-30 18:26:18 +0100

    front end showing now, requires fixes

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import NavBar from "./NavBar";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserAstronaut } from "@fortawesome/free-solid-svg-icons";
-import "./Profile.css";
-import { Button, Drawer, Space } from "antd";
-import { getProfile, addAddress } from "../apiService";
-import { useEffect, useState } from "react";
-import { Input } from "@material-tailwind/react";
-import { myWardrobe } from "../apiService";
-import SellitemProfile from "../components/SellItemProfile";
-import ItemProfile from "../components/ItemProfile";
+import React from 'react';
+import NavBar from './NavBar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserAstronaut } from '@fortawesome/free-solid-svg-icons';
+import './Profile.css';
+import { Button, Drawer, Space } from 'antd';
+import { AddAddressFunction } from '../apiService';
+import { useEffect, useState } from 'react';
+import { Input } from '@material-tailwind/react';
+import { GetUserItemsFunction } from '../apiService';
+import SellitemProfile from '../components/SellItemProfile';
+import ItemProfile from '../components/ItemProfile';
 
 interface Props {
   toggleComponent: () => void;
@@ -22,9 +22,9 @@ const Profile: React.FC<Props> = ({ toggleComponent, setSearchText }) => {
   const [name, setName] = useState(null);
   const [open, setOpen] = useState(false);
   const [houseNo, setHouseNo] = useState(null);
-  const [streetName, setStreetName] = useState("");
-  const [postCode, setPostCode] = useState("");
-  const [city, setCity] = useState("");
+  const [streetName, setStreetName] = useState('');
+  const [postCode, setPostCode] = useState('');
+  const [city, setCity] = useState('');
   const [items, setItems] = useState([]);
   const [drawerAddress, setDrawerAddress] = useState(false);
 
@@ -35,11 +35,11 @@ const Profile: React.FC<Props> = ({ toggleComponent, setSearchText }) => {
   // const staticUser = {};
 
   useEffect(() => {
-    renderProfile();
+    // renderProfile();
     renderProfile2();
   }, []);
 
-  const userId = localStorage.getItem("userId");
+  const userId = localStorage.getItem('userId');
 
   const handleCancel = () => {
     setOpen(false);
@@ -58,7 +58,7 @@ const Profile: React.FC<Props> = ({ toggleComponent, setSearchText }) => {
       postCode: postCode,
       city: city,
     };
-    const user = await addAddress(data, userId);
+    const user = await AddAddressFunction(data);
     setHouseNo(user.houseNo);
     setStreetName(user.streetName);
     setPostCode(user.postCode);
@@ -66,15 +66,15 @@ const Profile: React.FC<Props> = ({ toggleComponent, setSearchText }) => {
     setDrawerAddress(false);
   };
 
-  async function renderProfile() {
-    const user = await getProfile(userId);
-    setUserName(user.userName);
-    setUserEmail(user.email);
-    setName(user.name);
-  }
+  // async function renderProfile() {
+  //   const user = await getProfile(userId);
+  //   setUserName(user.userName);
+  //   setUserEmail(user.email);
+  //   setName(user.name);
+  // }
 
   const renderProfile2 = () => {
-    myWardrobe(userId).then((data) => {
+    GetUserItemsFunction().then((data) => {
       const sortedItems = data.sort((a, b) => {
         return (
           new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()
@@ -86,41 +86,41 @@ const Profile: React.FC<Props> = ({ toggleComponent, setSearchText }) => {
   };
 
   return (
-    <div className="profile-view">
+    <div className='profile-view'>
       <NavBar toggleComponent={toggleComponent} setSearchText={setSearchText} />
-      <div className=" flex p-24">
+      <div className=' flex p-24'>
         <FontAwesomeIcon
           icon={faUserAstronaut}
-          className="userIcon bg-orange-50 shadow-2xl m-24 mr-20 p-12 mt-2 mr-9 text-9xl text-slate-300 rounded-full"
+          // className='userIcon bg-orange-50 shadow-2xl m-24 mr-20 p-12 mt-2 mr-9 text-9xl text-slate-300 rounded-full'
         />
-        <div className="w-2/5 rounded-2xl shadow-2xl bg-orange-50 relative ml-20">
-          <h1 className="text-3xl p-10 font-bold">{userName}</h1>
-          <div className="flex justify-start ">
+        <div className='w-2/5 rounded-2xl shadow-2xl bg-orange-50 relative ml-20'>
+          <h1 className='text-3xl p-10 font-bold'>{userName}</h1>
+          <div className='flex justify-start '>
             <div>
-              <ul className="mr-20 ml-10 mt-5">
+              <ul className='mr-20 ml-10 mt-5'>
                 <li>{name}</li>
                 <li>{userEmail}</li>
               </ul>
             </div>
             <div>
-              <ul className="flex flex-col w-40">
-                <li className="font-bold">Address:</li>
-                <li className=" flex justify-between">
+              <ul className='flex flex-col w-40'>
+                <li className='font-bold'>Address:</li>
+                <li className=' flex justify-between'>
                   House No: <span>{houseNo}</span>
                 </li>
-                <li className="flex justify-between">
+                <li className='flex justify-between'>
                   Street: <span>{streetName}</span>
                 </li>
-                <li className="flex justify-between">
-                  Post Code:<span>{postCode}</span>{" "}
+                <li className='flex justify-between'>
+                  Post Code:<span>{postCode}</span>{' '}
                 </li>
-                <li className="flex justify-between">
+                <li className='flex justify-between'>
                   City:<span>{city}</span>
                 </li>
               </ul>
             </div>
             <div
-              className="mt-6 p-2 text-white bg-green-900 absolute rounded-lg right-10 top-5 hover:cursor-pointer"
+              className='mt-6 p-2 text-white bg-green-900 absolute rounded-lg right-10 top-5 hover:cursor-pointer'
               onClick={showDrawerAddress}
             >
               Add Details
@@ -128,23 +128,23 @@ const Profile: React.FC<Props> = ({ toggleComponent, setSearchText }) => {
           </div>
         </div>
       </div>
-      <div className="border w-4/5 ml-40"></div>
-      <div className="flex">
-        <h1 className="ml-28 mt-5 ">Your Warderobe</h1>{" "}
+      <div className='border w-4/5 ml-40'></div>
+      <div className='flex'>
+        <h1 className='ml-28 mt-5 '>Your Warderobe</h1>{' '}
         <button
-          className="ml-5 mt-4 ring-2 ring-amber-900 bg-amber-900 text-white rounded-md p-1 self-center mr-12 hover:cursor-pointer"
+          className='ml-5 mt-4 ring-2 ring-amber-900 bg-amber-900 text-white rounded-md p-1 self-center mr-12 hover:cursor-pointer'
           onClick={showDrawer}
         >
           Add
         </button>
       </div>
-      <div className="flex ml-6 mr-6 overflow-auto">
+      <div className='flex ml-6 mr-6 overflow-auto'>
         {items.map((item) => (
           <ItemProfile key={item._id} item={item} />
         ))}
       </div>
       <Drawer
-        title="Add Address"
+        title='Add Address'
         width={320}
         onClose={handleCancel}
         open={drawerAddress}
@@ -154,41 +154,41 @@ const Profile: React.FC<Props> = ({ toggleComponent, setSearchText }) => {
           </Space>
         }
       >
-        <form className="mt-10 flex mb-10">
-          <div className="flex flex-col mr-10 gap-6">
+        <form className='mt-10 flex mb-10'>
+          <div className='flex flex-col mr-10 gap-6'>
             <Input
               value={houseNo}
-              type="number"
-              label="House Number"
-              className="bg-white"
+              type='number'
+              label='House Number'
+              className='bg-white'
               onChange={(e) => setHouseNo(e.target.value)}
             />
             <Input
               value={streetName}
-              label="Street"
-              className="bg-white"
+              label='Street'
+              className='bg-white'
               onChange={(e) => setStreetName(e.target.value)}
             />
             <Input
               value={postCode}
-              label="Post code"
-              className="bg-white"
+              label='Post code'
+              className='bg-white'
               onChange={(e) => setPostCode(e.target.value)}
             />
             <Input
               value={city}
-              label="City"
-              className="bg-white"
+              label='City'
+              className='bg-white'
               onChange={(e) => setCity(e.target.value)}
             />
           </div>
         </form>
-        <Button className=" bg-green-900  text-white" onClick={handleUpdate}>
+        <Button className=' bg-green-900  text-white' onClick={handleUpdate}>
           Update
         </Button>
       </Drawer>
       <Drawer
-        title="Add Item"
+        title='Add Item'
         width={520}
         onClose={handleCancel}
         open={open}
--- client/src/components/Sellitem.tsx
+++ client/src/components/Sellitem.tsx
@@ -1,22 +1,21 @@
-import React from "react";
-import "./Sellitem.css";
-import { useState } from "react";
-import { useRef } from "react";
-import { Textarea, Input, Select, Option } from "@material-tailwind/react";
-import { addItem, sendImage } from "../apiService";
-
+import React from 'react';
+import './Sellitem.css';
+import { useState } from 'react';
+import { useRef } from 'react';
+import { Textarea, Input, Select, Option } from '@material-tailwind/react';
+import { PostItemFunction } from '../apiService';
 
 const Item: React.FC = () => {
-  const [titleInput, setTitleInput] = useState("");
-  const [descInput, setDescInput] = useState("");
-  const [priceInput, setPriceInput] = useState("");
-  const [condInput, setCondInput] = useState("");
-  const [catInput, setCatInput] = useState("");
-  const [sizeInput, setSizeInput] = useState("");
+  const [titleInput, setTitleInput] = useState('');
+  const [descInput, setDescInput] = useState('');
+  const [priceInput, setPriceInput] = useState('');
+  const [condInput, setCondInput] = useState('');
+  const [catInput, setCatInput] = useState('');
+  const [sizeInput, setSizeInput] = useState('');
   const [previewSource, setPreviewSource] = useState(null);
 
   const inputFile = useRef<HTMLInputElement>();
-  const userId = localStorage.getItem("userId");
+  const userId = localStorage.getItem('userId');
 
   function handleDrop(e) {
     e.preventDefault();
@@ -43,7 +42,7 @@ const Item: React.FC = () => {
     const imageData = {
       img: previewSource,
     };
-    sendImage(imageData);
+    // sendImage(imageData);
   };
 
   const handleUploadBtn = async (e) => {
@@ -58,95 +57,94 @@ const Item: React.FC = () => {
       size: sizeInput,
     };
 
-    await addItem(formItem, userId);
+    await PostItemFunction(formItem);
 
-
-    setCatInput("");
-    setCondInput("");
-    setDescInput("");
-    setPriceInput("");
-    setTitleInput("");
-    setSizeInput("");
+    setCatInput('');
+    setCondInput('');
+    setDescInput('');
+    setPriceInput('');
+    setTitleInput('');
+    setSizeInput('');
     setPreviewSource(null);
   };
 
   return (
     <>
       {previewSource && (
-        <img src={previewSource} alt="preview image" className="h-40" />
+        <img src={previewSource} alt='preview image' className='h-40' />
       )}
-      <input ref={inputFile} type="file" name="image" onChange={handleDrop} />
+      <input ref={inputFile} type='file' name='image' onChange={handleDrop} />
       <form onSubmit={handleSubmitFile}></form>
 
-      <form className="mt-10 flex mb-10">
-        <div className="flex flex-col mr-10 gap-6">
+      <form className='mt-10 flex mb-10'>
+        <div className='flex flex-col mr-10 gap-6'>
           <Input
             value={titleInput}
-            label="Title"
-            className="bg-white  "
+            label='Title'
+            className='bg-white  '
             onChange={(e) => setTitleInput(e.target.value)}
           />
           <Textarea
             value={descInput}
-            label="Description..."
-            className="bg-white "
+            label='Description...'
+            className='bg-white '
             onChange={(e) => setDescInput(e.target.value)}
           />
         </div>
-        <div className="flex flex-col gap-6">
-          <div className="flex items-baseline">
-            <h1 className="mr-1">£</h1>
+        <div className='flex flex-col gap-6'>
+          <div className='flex items-baseline'>
+            <h1 className='mr-1'>£</h1>
             <Input
               value={priceInput}
-              label="Price"
-              type="number"
-              className="mb-5 bg-white "
+              label='Price'
+              type='number'
+              className='mb-5 bg-white '
               onChange={(e) => setPriceInput(e.target.value)}
             />
           </div>
           <Select
-            label="Select Size"
+            label='Select Size'
             value={sizeInput}
-            className="mb-5 bg-white "
+            className='mb-5 bg-white '
             onChange={(e) => setSizeInput(e)}
           >
-            <Option value="S">S</Option>
-            <Option value="M">M</Option>
-            <Option value="L">L</Option>
-            <Option value="XL">XL</Option>
-            <Option value="XXL">XXL</Option>
-            <Option value="0-3YO">0-3YO</Option>
-            <Option value="4-6YO">4-6YO</Option>
-            <Option value="7-9YO">7-9YO</Option>
-            <Option value="10-12YO">10-12YO</Option>
+            <Option value='S'>S</Option>
+            <Option value='M'>M</Option>
+            <Option value='L'>L</Option>
+            <Option value='XL'>XL</Option>
+            <Option value='XXL'>XXL</Option>
+            <Option value='0-3YO'>0-3YO</Option>
+            <Option value='4-6YO'>4-6YO</Option>
+            <Option value='7-9YO'>7-9YO</Option>
+            <Option value='10-12YO'>10-12YO</Option>
           </Select>
 
           <Select
-            label="Select Category"
+            label='Select Category'
             value={catInput}
-            className="mb-5 bg-white "
+            className='mb-5 bg-white '
             onChange={(e) => setCatInput(e)}
           >
-            <Option value="Men">Men</Option>
-            <Option value="Women">Women</Option>
-            <Option value="Kids">Kids</Option>
+            <Option value='Men'>Men</Option>
+            <Option value='Women'>Women</Option>
+            <Option value='Kids'>Kids</Option>
           </Select>
 
           <Select
             value={condInput}
-            label="Condition"
-            className="bg-white"
+            label='Condition'
+            className='bg-white'
             onChange={(e) => setCondInput(e)}
           >
-            <Option value="New">New</Option>
-            <Option value="Like New">Like New</Option>
-            <Option value="Good">Good</Option>
-            <Option value="Used">Used</Option>
+            <Option value='New'>New</Option>
+            <Option value='Like New'>Like New</Option>
+            <Option value='Good'>Good</Option>
+            <Option value='Used'>Used</Option>
           </Select>
         </div>
       </form>
       <button
-        className="bg-green-900 rounded-lg p-3 px-20 text-white"
+        className='bg-green-900 rounded-lg p-3 px-20 text-white'
         onClick={handleUploadBtn}
       >
         Upload
@@ -156,4 +154,3 @@ const Item: React.FC = () => {
 };
 
 export default Item;
-
